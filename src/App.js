142deb0f463d03da0be7ecff0601dc59726c1380
NabajitS <86390740+NabajitS@users.noreply.github.com>
2022-12-29 16:17:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [info, setInfo] = useState(null)
+
+  useEffect(() => {
+
+    const getData = async () => {
+      // const response = await fetch('http://localhost:8000')
+      const response = await fetch('https://dummydeploy-api.onrender.com')
+
+      const data = await response.json();
+      console.log(data.dataSend)
+      setInfo(data.dataSend)
+
+    }
+
+    if (true) getData();
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
+
+      <div>
+        {
+          info && info.map((inf) => <>
+            <h1>{inf.id}</h1>
+            <h1>{inf.name}</h1>
+          </>)
+        }
+
+      </div>
+
     </div>
   );
 }
